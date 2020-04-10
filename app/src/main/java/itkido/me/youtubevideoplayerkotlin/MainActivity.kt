441c04695b069a089f3e1cc05b30de454a41ffc8
Martin Spusta <lucas.spusta@gmail.com>
2020-04-10 09:24:21 -0400

    Initial commit

---
+++ app/src/main/java/itkido/me/youtubevideoplayerkotlin/MainActivity.kt
@@ -0,0 +1,26 @@
+package itkido.me.youtubevideoplayerkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
+        setContentView(R.layout.activity_main)
+
+        lifecycle.addObserver(youtube_player_view)
+
+        youtube_player_view.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId = "S0Q4gqBUs7c"
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
+    }
+}
